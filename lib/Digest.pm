4a82c7b394f620bf4bae0512308b6f0d236e886c
Gisle Aas <gisle@aas.no>
1998-10-26 11:48:43 +0000

    Some autoloading.

--- lib/Digest.pm
+++ lib/Digest.pm
@@ -1,14 +1,36 @@
 package Digest;
 
+use strict;
+use vars qw($VERSION %MMAP $AUTOLOAD);
+
 $VERSION = "0.01";
 
-sub new;
-sub reset;
-sub add;
-sub addfile;
-sub digest;
-sub hexdigest;
-sub b64digest;
+%MMAP = (
+  "SHA-1"      => "Digest::SHA1",
+  "HMAC-MD5"   => "Digest::HMAC_MD5",
+  "HMAC-SHA-1" => "Digest::HMAC_SHA1",
+);
+
+sub new
+{
+    shift;  # class ignored
+    my $algorithm = shift;
+    my $class = $MMAP{$algorithm} || "Digest::$algorithm";
+    no strict 'refs';
+    unless (%{"$class\::"}) {
+	eval "require $class";
+	die $@ if $@;
+    }
+    $class->new(@_);
+}
+
+sub AUTOLOAD
+{
+    my $class = shift;
+    my $algorithm = substr($AUTOLOAD, rindex($AUTOLOAD, '::')+2);
+    $class->new($algorithm, @_);
+}
 
 1;
 
+__END__
