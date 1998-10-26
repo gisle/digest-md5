8defbd2fcb3f3d174ba4de1a2151a4a8f6859c14
Gisle Aas <gisle@aas.no>
1998-10-26 00:24:05 +0000

    Working implementation.

--- lib/Digest/SHA1.pm
+++ lib/Digest/SHA1.pm
@@ -1,6 +1,8 @@
 package Digest::SHA1;
 
-# This only demonstrates the interface
+# This demonstrates what need to be done to conform to the same
+# interface as Digest::MD5 (and Digest::HMAC).  This should really be
+# implemented similar to how Digest::MD5 is, and SHA made into a stub.
 
 $VERSION = '0.01';
 
@@ -10,7 +12,59 @@ my $sha_ver = SHA::sha_version();
 die "Wrong SHA version ($sha_ver)" unless $sha_ver eq "SHA-1";
 
 sub new {
-    SHA->new;
+    my $class = shift;
+    my $self = SHA->new;
+    bless \$self, $class;
 }
+
+sub reset
+{
+    my $self = shift;
+    $$self->reset(@_);
+    $self;
+}
+
+sub add
+{
+    my $self = shift;
+    $$self->add(@_);
+    $self;
+}
+
+sub addfile
+{
+    my $self = shift;
+    $$self->add(@_);
+    $self;
+}
+
+sub digest
+{
+    my $self = shift;
+    $$self->digest(@_);
+}
+
+sub hexdigest
+{
+    my $self = shift;
+    $$self->hexdigest(@_);
+}
+
+sub b64digest
+{
+    require MIME::Base64;
+    my $digest = MIME::Base64::encode(shift->digest, '');
+    $digest =~ s/=+$//;
+    $digest;
+}
+
+# Functional interface
+require Exporter;
+*import = \&Exporter::import;
+@EXPORT_OK = qw(sha1 sha1_hex sha1_base64);
+
+sub sha1          {  Digest::SHA1->new->add(@_)->digest;    }
+sub sha1_hex      {  Digest::SHA1->new->add(@_)->hexdigest; }
+sub sha1_basse64  {  Digest::SHA1->new->add(@_)->b64digest; }
 
 1;
