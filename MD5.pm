ef42ac738a4ecc28f5da2858aaa8d76508a6943d
Gisle Aas <gisle@aas.no>
1999-07-28 10:29:23 +0000

    Incremented version number.

--- MD5.pm
+++ MD5.pm
@@ -3,7 +3,7 @@ package Digest::MD5;
 use strict;
 use vars qw($VERSION @ISA @EXPORT_OK);
 
-$VERSION = '2.07';  # $Date$
+$VERSION = '2.08';  # $Date$
 
 require Exporter;
 *import = \&Exporter::import;
