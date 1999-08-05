d0edf6ca96e24355cc5c551843c86f992a3b8f1f
Gisle Aas <gisle@aas.no>
1999-08-05 22:58:42 +0000

    Increment version number.

--- SHA1/SHA1.pm
+++ SHA1/SHA1.pm
@@ -3,7 +3,7 @@ package Digest::SHA1;
 use strict;
 use vars qw($VERSION @ISA @EXPORT_OK);
 
-$VERSION = '1.01';  # $Date$
+$VERSION = '1.02';  # $Date$
 
 require Exporter;
 *import = \&Exporter::import;
