1d2abe4e013b6e5865779b7d014ddaae125ff9bd
Gisle Aas <gisle@aas.no>
2003-12-05 10:11:49 +0000

    Release 2.32.

--- MD5.pm
+++ MD5.pm
@@ -3,7 +3,7 @@ package Digest::MD5;
 use strict;
 use vars qw($VERSION @ISA @EXPORT_OK);
 
-$VERSION = '2.31';  # $Date$
+$VERSION = '2.32';  # $Date$
 
 require Exporter;
 *import = \&Exporter::import;
